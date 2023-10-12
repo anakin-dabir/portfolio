b90334b2777df4dc5cf97c7e0c3d96fc83ff71b9
anakin-dabir <anakindabir@gmail.com>
2023-10-12 07:32:49 +0500

    Adding Error.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from 'react';
 const Home = React.lazy(() => import('./pages/Home'));
 const About = React.lazy(() => import('./pages/About'));
+// import About from './pages/About';
 const Projects = React.lazy(() => import('./pages/Projects'));
 const Contact = React.lazy(() => import('./pages/Contact'));
-import Test from './Test';
+import Error from './pages/Error';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Wrapper from './components/Wrapper';
 import Loader from './components/Loader';
@@ -18,7 +19,7 @@ const App = () => {
 							<Route path='/about' element={<About />} />
 							<Route path='/projects' element={<Projects />} />
 							<Route path='/contact' element={<Contact />} />
-							<Route path='/*' element={<Test />} />
+							<Route path='/*' element={<Error />} />
 						</Routes>
 					</Suspense>
 				</Wrapper>
