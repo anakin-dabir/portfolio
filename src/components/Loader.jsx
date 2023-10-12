cf6d7ce73affbfed6b342d694b4ddd2e6bbff73f
anakin-dabir <anakindabir@gmail.com>
2023-10-12 07:46:17 +0500

    Fixed Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
 	return (
-		<div className='fixed pt-14 md:pt-0 top-1/2 left-1/2 box-center flex-col -translate-x-1/2 -translate-y-1/2'>
+		<div className='fixed opacity-70 pt-14 md:pt-0 top-1/2 left-1/2 box-center flex-col -translate-x-1/2 -translate-y-1/2'>
 			<div className='running md:scale-[2] scale-150'>
 				<div className='outer'>
 					<div className='body'>
