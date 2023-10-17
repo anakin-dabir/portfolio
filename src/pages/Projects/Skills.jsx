629a1be4651912df7cbd11178a676e2d1d60af9d
anakin-dabir <anakindabir@gmail.com>
2023-10-17 14:03:08 +0500

    Adding Projects.jsx

---
+++ src/pages/Projects/Skills.jsx
@@ -0,0 +1,86 @@
+import { CssIcon, FigmaIcon, HtmlIcon, ReactIcon } from './icons/IconPack1';
+import { ExpressIcon, JsIcon, MongoIcon, TsIcon } from './icons/IconPack2';
+import {
+	MuiIcon,
+	NextIcon,
+	ReduxIcon,
+	SocketIcon,
+	TailwindIcon,
+} from './icons/IconPack3';
+import { FireBaseIcon, WebRTCIcon } from './icons/IconPack4';
+import { CIcon, RechartIcon } from './icons/IconPack5';
+
+const Skills = ({ height }) => {
+	return (
+		<div style={{ height: height }} className={`overflow-y-auto`}>
+			<div className='box-center xl:content-center flex-wrap p-4 h-full gap-5'>
+				<SvgLink to='https://react.dev/' name='React'>
+					<ReactIcon />
+				</SvgLink>
+				<SvgLink to='https://nextjs.org/' name='Next'>
+					<NextIcon />
+				</SvgLink>
+				<SvgLink to='https://en.wikipedia.org/wiki/JavaScript' name='JavaScript'>
+					<JsIcon />
+				</SvgLink>
+				<SvgLink to='https://www.typescriptlang.org/' name='TypeScript'>
+					<TsIcon />
+				</SvgLink>
+				<SvgLink to='https://expressjs.com/' name='ExpressJs'>
+					<ExpressIcon />
+				</SvgLink>
+				<SvgLink to='https://www.mongodb.com/docs/' name='MongoDB'>
+					<MongoIcon />
+				</SvgLink>
+				<SvgLink to='https://tailwindcss.com/' name='TailwindCSS'>
+					<TailwindIcon />
+				</SvgLink>
+				<SvgLink to='https://mui.com/' name='MaterialUI'>
+					<MuiIcon />
+				</SvgLink>
+				<SvgLink to='https://en.wikipedia.org/wiki/HTML' name='HTML'>
+					<HtmlIcon />
+				</SvgLink>
+				<SvgLink to='https://en.wikipedia.org/wiki/CSS' name='CSS'>
+					<CssIcon />
+				</SvgLink>
+				<SvgLink to='https://redux.js.org/' name='Redux'>
+					<ReduxIcon />
+				</SvgLink>
+				<SvgLink to='https://webrtc.org/' name='WebRTC'>
+					<WebRTCIcon />
+				</SvgLink>
+				<SvgLink to='https://firebase.google.com/' name='FireBase'>
+					<FireBaseIcon />
+				</SvgLink>
+				<SvgLink to='https://socket.io/' name='Socket.io'>
+					<SocketIcon />
+				</SvgLink>
+				<SvgLink to='https://recharts.org/' name='<Recharts/>'>
+					<RechartIcon />
+				</SvgLink>
+				<SvgLink to='https://www.figma.com/' name='Figma'>
+					<FigmaIcon />
+				</SvgLink>
+				<SvgLink to='https://en.wikipedia.org/wiki/C%2B%2B' name='C++'>
+					<CIcon />
+				</SvgLink>
+			</div>
+		</div>
+	);
+};
+
+export default Skills;
+
+const SvgLink = ({ to, name, children }) => {
+	return (
+		<>
+			<a href={to} className='box-center group relative' target='_blank' rel='noopener noreferrer'>
+				<div className='bg-black border border-borderColor h-5 opacity-0 box-center transition-opacity group-focus:opacity-100 text-textInactive absolute top-0 right-0 group-hover:opacity-100'>
+					{name}
+				</div>
+				{children}
+			</a>
+		</>
+	);
+};
--- src/pages/Projects/data.jsx
+++ src/pages/Projects/data.jsx
@@ -1,7 +1,35 @@
+import React from 'react';
+import Loader from '../../components/Loader';
+const Skills = React.lazy(() => import('./Skills'));
+
+const SkillsData = ({ height }) => {
+	return (
+		<>
+			<React.Suspense fallback={<Loader />}>
+				<Skills height={height} />
+			</React.Suspense>
+		</>
+	);
+};
+
 const data = [
 	{
-		title: 'Personal Info',
-		content: [{ fileName: 'Personal Info' }],
+		title: 'Projects',
+		content: [{ fileName: 'Full Stack' }, { fileName: 'Front End' }, { fileName: 'html/css' }],
+	},
+	{
+		title: 'Skills',
+		content: [
+			{
+				fileName: 'skills',
+				fileType: 'Component',
+				fileContent: SkillsData,
+			},
+		],
+	},
+	{
+		title: 'Certifications1',
+		content: [{ fileName: 'skills' }],
 	},
 ];
 
