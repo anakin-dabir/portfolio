1e1a121b1c70de0da1503beb1b9976e3e0947735
anakin-dabir <anakindabir@gmail.com>
2023-10-25 00:53:19 +0500

    Adding Projects & fixing issues

--- src/pages/Projects/data/Portal.jsx
+++ src/pages/Projects/data/Portal.jsx
@@ -18,23 +18,28 @@ const Portal = ({isOpen, setOpen, data, index}) => {
   return createPortal(
     <>
       {isOpen === index && (
-        <div className='fixed inset-0 z-0' onClick={handleClose}>
+        <div className='fixed inset-0 z-50' onClick={handleClose}>
           <div className='fixed inset-0 bg-black opacity-60'></div>
         </div>
       )}
       <div
         ref={ref}
-        className={`h-[80vh] overflow-y-auto w-[75vw] top-1/2 z-40 left-1/2 -translate-x-1/2 flex-col flex -translate-y-1/2 fixed ${
+        className={`h-[80vh] w-[90vw] md:h-[80vh] overflow-y-auto flex-col md:w-[75vw] top-1/2 z-[60] left-1/2 -translate-x-1/2 -translate-y-1/2 fixed ${
           isOpen === index ? 'animator' : 'animator1'
         }  bg-borderColor ${isOpen === index ? 'flex' : ''}`}
       >
-        {data.name}
-        <button
-          className='box-center border border-borderActive w-40 h-10 rounded-lg hover:bg-borderActive transition-colors hover:text-black'
-          onClick={handleClose}
-        >
-          Close
-        </button>
+        <div className='flex items-center flex-col h-full w-full p-4'>
+          {/* <div className='h-60 w-full'> */}
+          <img src={data.src} className='h-full w-full object-contain' />
+          {/* </div> */}
+        </div>
+        <div className='flex flex-col'>
+          <span>{`{`}</span>
+          <span>{`  name: "Wons Dashboard",`}</span>
+          <span>{`  stack: "React, MUI, ExpressJS",`}</span>
+          <span>{`  stack: "React, MUI, ExpressJS",`}</span>
+          <span>{`}`}</span>
+        </div>
       </div>
     </>,
     document.getElementById('modal')
--- src/pages/Projects/data/Project.jsx
+++ src/pages/Projects/data/Project.jsx
@@ -4,7 +4,7 @@ import Portal from './Portal';
 const Project = () => {
   return (
     <>
-      <div class='grid w-full md:grid-rows-10 md:grid-cols-3 gap-3'>
+      <div class='grid w-full p-10 md:grid-rows-10 md:grid-cols-2 gap-3'>
         <ProjectItems data={data} />
       </div>
     </>
@@ -27,15 +27,15 @@ const ProjectItems = ({data}) => {
     <>
       {data.map((data, i) => {
         return (
-          <div key={i} className={`${data.className} group`}>
-            <button onClick={() => handleItemClick(i)} className='h-full w-full'>
+          <div key={i} className={`${data.className}bg-dark`}>
+            <button onClick={() => handleItemClick(i)} className='group h-full w-full'>
               <div className='h-full w-full relative overflow-hidden'>
                 <img
                   src={data.src}
-                  className='group-hover:scale-150 h-full w-full object-cover opacity-50 transition-transform'
+                  className='group-hover:scale-150 h-full w-full object-contain opacity-100  transition-transform'
                 />
                 <div className='absolute left-0 bg-black inset-0 opacity-0 group-hover:opacity-70 transition-opacity h-full w-full top-0'></div>
-                <div className='absolute h-10 w-10 right-0 top-0 hidden group-hover:flex cursor-pointer'></div>
+                {/* <div className='absolute h-40 bg-black w-full bottom-0 group-hover:flex cursor-pointer'></div> */}
               </div>
             </button>
             <Portal isOpen={activeIndex} setOpen={handleItemClick} index={i} data={data} />
@@ -48,13 +48,14 @@ const ProjectItems = ({data}) => {
 
 const data = [
   {
-    className: 'w-full h-96 md:col-span-2 bg-black',
-    src: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D',
+    className: 'w-full bg-black col-span-2',
+    src: 'https://themewagon.com/wp-content/uploads/2021/11/purple-react-1.png',
     name: 'SnakeGame',
+    stack: 'React, MongoDB, ExpressJS, JWT, Recharts, Tailwindcss',
   },
   {
-    className: 'w-full md:row-span-2 bg-black',
-    src: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8fDA%3D',
+    className: 'w-full bg-black col-span-2',
+    src: 'https://themewagon.com/wp-content/uploads/2021/11/purple-react-1.png',
     name: 'SnakeGame2',
   },
   {
