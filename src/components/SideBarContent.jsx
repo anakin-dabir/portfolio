a78381cafb4f207a52f8b162c06c5b66152e678e
anakin-dabir <anakindabir@gmail.com>
2024-03-06 05:12:48 +0500

    fix: setting max-h-48

--- src/components/SideBarContent.jsx
+++ src/components/SideBarContent.jsx
@@ -2,7 +2,7 @@ import { FileIcon, TxtIcon } from "./Icons";
 
 const SideBarContent = ({ title, content, onClick, isActive, handleFileClick, file, index }) => {
   return (
-    <div className="w-full flex flex-col">
+    <div className='w-full flex flex-col'>
       <button
         onClick={onClick}
         className={`items-center bg-borderColor/80 focus:bg-borderColor/80 px-3 text-sm h-10  w-full  flex`}
@@ -13,15 +13,15 @@ const SideBarContent = ({ title, content, onClick, isActive, handleFileClick, fi
           } transition-colors box-center`}
         >
           {isActive ? <FileIcon opened /> : <FileIcon />}
-          <p className="pl-3">{title}</p>
+          <p className='pl-3'>{title}</p>
         </div>
       </button>
       <div
         className={`overflow-hidden mb-[3px] bg-dark transition-all ease-in-out duration-500 ${
-          isActive ? "max-h-56" : "max-h-0"
+          isActive ? "max-h-48" : "max-h-0"
         }`}
       >
-        <div className="flex flex-col gap-4 py-3 pl-3">
+        <div className='flex flex-col gap-4 py-3 pl-3'>
           {content.map((content, i) => {
             const { icon: Icon } = content;
             return (
